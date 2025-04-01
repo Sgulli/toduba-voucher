443f5ba64f95c16e3adf0368bec72629cd46ffce
sgulli <sguglielmi40@gmail.com>
2025-04-01 23:30:40 +0200

    chore: update userService to usersService in routes

--- src/users/users.route.ts
+++ src/users/users.route.ts
@@ -1,30 +1,30 @@
 import express from "express";
-import { userService } from "./users.service";
+import { usersService } from "./users.service";
 
 const router = express.Router();
 
 router.get("/", async (_, res) => {
-  const users = await userService.getAll();
+  const users = await usersService.getAll();
   res.status(200).jsonp(users);
 });
 
 router.get("/:id", async (req, res) => {
-  const user = await userService.get(req.params.id);
+  const user = await usersService.get(req.params.id);
   res.status(200).jsonp(user);
 });
 
 router.post("/", async (req, res) => {
-  const user = await userService.create(req.body);
+  const user = await usersService.create(req.body);
   res.status(201).jsonp(user);
 });
 
 router.patch("/:id", async (req, res) => {
-  const user = await userService.update(req.params.id, req.body);
+  const user = await usersService.update(req.params.id, req.body);
   res.status(200).jsonp(user);
 });
 
 router.delete("/:id", async (req, res) => {
-  const user = await userService.delete(req.params.id);
+  const user = await usersService.delete(req.params.id);
   res.status(200).jsonp(user);
 });
 

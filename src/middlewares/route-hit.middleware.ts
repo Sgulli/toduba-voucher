aeb6c7a3ab7a2a44ea9afaefc092c41228cc5f68
sgulli <sguglielmi40@gmail.com>
2025-04-08 21:46:35 +0200

    small improvement

--- src/middlewares/route-hit.middleware.ts
+++ src/middlewares/route-hit.middleware.ts
@@ -3,10 +3,10 @@ import { RequestHandler } from "express";
 const requestWithBody = new Set(["POST", "PUT", "PATCH", "DELETE"]);
 
 export const routeHitHandler: RequestHandler = (req, res, next) => {
-  const { method, originalUrl } = req;
+  const { method, originalUrl, body } = req;
   req.log.info({ name: "Server" }, `${method} ${originalUrl}`);
 
-  if (requestWithBody.has(method) && Object.keys(req.body).length) {
+  if (requestWithBody.has(method) && Object.keys(body).length) {
     req.log.info({ name: "Server" }, `Body: ${JSON.stringify(req.body)}`);
   }
   next();

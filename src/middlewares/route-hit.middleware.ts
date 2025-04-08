b040310a7980879799fe7494a7d6b72e17e766f7
sgulli <sguglielmi40@gmail.com>
2025-04-08 21:45:51 +0200

    feat: enhance route hit logging to include request body for relevant methods

--- src/middlewares/route-hit.middleware.ts
+++ src/middlewares/route-hit.middleware.ts
@@ -1,7 +1,13 @@
 import { RequestHandler } from "express";
 
+const requestWithBody = new Set(["POST", "PUT", "PATCH", "DELETE"]);
+
 export const routeHitHandler: RequestHandler = (req, res, next) => {
   const { method, originalUrl } = req;
   req.log.info({ name: "Server" }, `${method} ${originalUrl}`);
+
+  if (requestWithBody.has(method) && Object.keys(req.body).length) {
+    req.log.info({ name: "Server" }, `Body: ${JSON.stringify(req.body)}`);
+  }
   next();
 };
